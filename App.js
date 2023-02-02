6f03fd15fd87221c57d0ffcddf426aaf5c360245
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-02 16:38:21 +0000

    the first app v3

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working onyour app! yes v3</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+
+      <View style={styles.headere}>
+        <Text style={styles.headere} >First app</Text>
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.boldText}>Hello, World from react native!</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Lorem ipsum <Text style={styles.boldText}>dolor</Text> sit amet.</Text>
+          <Text>Lorem ipsum dolor sit amet.</Text>
+          <Text>Lorem ipsum dolor sit amet.</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <Text >edited by ayoub </Text>
+      </View>
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'green',
+    padding: 40,
+  },
+  body: {
+    backgroundColor: 'pink',
+    padding: 50,
+    margin: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  footer: {
+    backgroundColor: "yellow"
+  },
+  headere: {
+    marginTop:20,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 20,
+
+  }
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+## simple screenshot
+
+![this is the first app :](./assets/screenshot_20230202_163624.png)
