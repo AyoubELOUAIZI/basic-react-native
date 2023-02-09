6b2c58e8c2076fef609fb9fc815d6df93f19fd29
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-09 02:52:44 +0000

    using the flatList

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('ahmed');
-  const [age, setAge] = useState('30');
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <View style={styles.mybloc}>
-        <Text>Enter name:</Text>
-        <TextInput
-          placeholder='your name'
-          style={styles.input}
-          onChangeText={(value) => setName(value)} />
-      </View>
-      <View style={styles.mybloc}>
-        <Text>Enter age:</Text>
-        <TextInput
-          placeholder='your age'
-          style={styles.input}
-          onChangeText={(value) => setAge(value)} />
-      </View>
 
-      <Text style={styles.result}>name: {name}, age: {age}</Text>
+      <FlatList
+        // numColumns={1}/
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
     </View>
   );
 }
@@ -30,25 +31,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'yellow',
-    // alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+    backgroundColor: '#ddf',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 2,
-    margin: 10,
-    width: 250,
-    borderRadius:8,
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   },
-  mybloc: {
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-evenly',
-
-  },
-  result:{
-    padding:10,
-  }
 });
